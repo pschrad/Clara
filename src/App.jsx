ae18f370cf6c96f128e016a462989615bab5b13c
Phyona Schrader <pschrad2@nd.edu>
2025-07-22 00:23:44 +0000

    Adding routing and background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Profile from './pages/Profile';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Profile from './pages/Profile.jsx';
+import Home from './pages/Home.jsx';
+import Gallery from './pages/Gallery.jsx';
+import Videos from './pages/Videos.jsx';
+import Contacts from './pages/Contacts.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-
-    <div>
-      <Profile />
-    </div>
-      
-  )
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/videos">Videos</Link>
+        <Link to="/contacts">Contacts</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
