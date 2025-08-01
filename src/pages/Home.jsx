a800ca37a2c1b7f528cd4a7c26bcb735b638e447
Phyona Schrader <pschrad2@nd.edu>
2025-08-01 03:24:02 +0000

    New working scrolling, want longer quote card, and no background photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-// src/Profile.jsx
-import './Home.css'; // Still using your own CSS or modules
+import './Home.css';
 import ProfilePic from './CaraProfile.png';
 import Logo from './logo.png';
 
@@ -12,31 +11,42 @@ function Home() {
           src={ProfilePic}
           alt="Foto de perfil"
           className="profile-image"
-          
         />
       </div>
 
-      {/* Right Side: Main Content */}
-      <div className="main-content">
+      {/* Right Side: Text Content */}
+      <div className="text-content">
         <h1 className="profile-name">Home</h1>
-        <p className="profile-role">This is a little bit about me... I play very pretty and and very pretty and dab. <br/>................................................................................................................</p>
+        <p className="profile-role">
+          This is a little bit about me... I play very pretty and and very pretty and dab. <br />
+          ................................................................................................................
+        </p>
         <p className="profile-description">
-          This is just a different paragraph.. We can make as many of these as you want</p>
+          This is just a different paragraph.. We can make as many of these as you want
+        </p>
+      </div>
 
-        <div className="quote-card">
-          <h2>Get a quote</h2>
+      {/* Quote Card */}
+      <div className="quote-card">
+        <h2>Get a quote</h2>
+      </div>
+
+      {/* Bottom Cards */}
+      <div className="bottom-cards">
+        <div className="bottom-cards-inner">
+          <div className="home-card">Card 1</div>
+          <div className="home-card">Card 2</div>
+          <div className="home-card">Card 3</div>
+          <div className="home-card">Card 1</div> {/* Duplicate for seamless loop */}
+          <div className="home-card">Card 2</div>
+          <div className="home-card">Card 3</div>
+          <div className="home-card">Card 1</div>
+          <div className="home-card">Card 2</div>
+          <div className="home-card">Card 3</div>
+          <div className="home-card">Card 1</div>
+          <div className="home-card">Card 2</div>
+          <div className="home-card">Card 3</div>
           
-        </div>
-
-       
-
-        <div className="bottom-cards">
-          <div className="home-card">Card 1</div>
-          <div className="home-card">Card 2</div>
-          <div className="home-card">Card 3</div>
-          <div className="home-card">Card 1</div>
-          <div className="home-card">Card 2</div>
-          <div className="home-card">Card 3</div>
         </div>
       </div>
     </div>
